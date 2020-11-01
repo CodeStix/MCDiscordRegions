ec37c8fdb7c9f787163c04353791c6ae65504af7
Stijn Rogiest <reddusted@gmail.com>
2020-11-01 11:21:38 +0100

    Added dotenv, debug, ws

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("This is a test");
+require("dotenv").config();
+import { Server as WebSocketServer } from "ws";
+import { debug } from "debug";
+
+const logger = debug("mc-discord-regions");
+
+logger("starting...");
+
+const server = new WebSocketServer({
+    port: process.env.PORT as any,
+});
+
+server.once("listening", () => {
+    logger(`websocket server is listening on port ${process.env.PORT}`);
+});
+
+server.once("connection", (client, req) => {
+    logger(`client connection from ${client.url}`);
+});
